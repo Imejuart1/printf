0760315d6844cb2880f1bbbdd81e8a89dec420b9
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-05-21 05:24:11 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,90 +1,160 @@
 #include "main.h"
-
-void cleanup(va_list args, buffer_t *output);
-int run_printf(const char *format, va_list args, buffer_t *output);
-int _printf(const char *format, ...);
+#include <stdarg.h>
 
 /**
- * cleanup - Peforms cleanup operations for _printf.
- * @args: A va_list of arguments provided to _printf.
- * @output: A buffer_t struct.
- */
-void cleanup(va_list args, buffer_t *output)
-{
-va_end(args);
-write(1, output->start, output->len);
-free_buffer(output);
-}
-
-/**
- * run_printf - Reads through the format string for _printf.
- * @format: Character string to print - may contain directives.
- * @output: A buffer_t struct containing a buffer.
- * @args: A va_list of arguments.
- *
- * Return: The number of characters stored to output.
- */
-int run_printf(const char *format, va_list args, buffer_t *output)
-{
-int i, wid, prec, ret = 0;
-char tmp;
-unsigned char flags, len;
-unsigned int (*f)(va_list, buffer_t *,
-unsigned char, int, int, unsigned char);
-
-for (i = 0; *(format + i); i++)
-{
-len = 0;
-if (*(format + i) == %)
-{
-tmp = 0;
-flags = handle_flags(format + i + 1, &tmp);
-wid = handle_width(args, format + i + tmp + 1, &tmp);
-prec = handle_precision(args, format + i + tmp + 1,
-&tmp);
-len = handle_length(format + i + tmp + 1, &tmp);
-
-f = handle_specifiers(format + i + tmp + 1);
-if (f != NULL)
-{
-i += tmp + 1;
-ret += f(args, output, flags, wid, prec, len);
-continue;
-}
-else if (*(format + i + tmp + 1) == 0)
-{
-ret = -1;
-break;
-}
-}
-ret += _memcpy(output, (format + i), 1);
-i += (len != 0) ? 1 : 0;
-}
-cleanup(args, output);
-return (ret);
-}
-
-/**
- * _printf - Outputs a formatted string.
- * @format: Character string to print - may contain directives.
- *
- * Return: The number of characters printed.
+ * _printf - function produces output according to a format.
+ * @format: is a pointer to string
+ * Return: is the count of printed characters
  */
 int _printf(const char *format, ...)
 {
-buffer_t *output;
-va_list args;
-int ret;
+const char *string;
 
-if (format == NULL)
-return (-1);
-output = init_buffer();
-if (output == NULL)
+int count = 0;
+
+va_list args;
+
+if (!format)
 return (-1);
 
 va_start(args, format);
+string = format;
 
-ret = run_printf(format, args, output);
+count = loop_format(string, args);
 
-return (ret);
+va_end(args);
+
+return (count);
+}
+
+/**
+ * loop_format - function is to print format
+ * @format: is a pointer to string
+ * @args: is a va_list args
+ * Return: is an integer.
+ */
+int loop_format(const char *format, va_list args)
+{
+int i = 0, counter = 0, flag = 0, check_flag = 0, f_counter = 0;
+
+while (i < _strlen((char *)format) && *(format + i) != 0)
+{
+char charac = format[i];
+
+if (charac == %)
+{
+flag++, i++;
+charac = format[i];
+if (charac == 0 && _strlen((char *)format) == 1)
+return (-1);
+if (charac == 0)
+return (counter);
+if (charac == %)
+flag++;
+else
+{
+f_counter = func_service(charac, args);
+if (f_counter >= 0 && f_counter != -1)
+{
+i++;
+charac = format[i];
+if (charac == %)
+flag--;
+counter += f_counter;
+}
+else if (f_counter == -1 && charac != n && flag == 1)
+counter += _putchar(%);
+}
+
+}
+check_flag = check_percent(&flag, charac);
+counter += check_flag;
+if (check_flag == 0 && charac != % && charac != 0)
+counter += _putchar(charac), i++;
+check_flag = 0;
+}
+return (counter);
+}
+
+
+/**
+ * check_percent - print a percentage
+ * @flag: is address of an int
+ * @charac: is a char
+ * Return: is 1 if % was printed, 0 otherwise.
+ */
+int check_percent(int *flag, char charac)
+{
+int count = 0;
+
+int tmp;
+
+tmp = *flag;
+
+if (tmp == 2 && charac == %)
+{
+count = _putchar(%);
+tmp = 0;
+}
+return (count);
+}
+
+
+/**
+ * func_service - takes arguments and prints it accordingly
+ * @charac: is a char or type determinant
+ * @args: is a va_list
+ * Return: is the number of characters printed
+ */
+int func_service(char charac, va_list args)
+{
+int count = 0;
+
+count = _switch(charac, args);
+
+return (count);
+}
+
+
+/**
+ * _switch - switch character to find arg
+ * @arg: is a va_list argument
+ * @c: is a char
+ * Return: count of printed characters
+ */
+int _switch(char c, va_list arg)
+{
+int count = 0;
+
+switch (c)
+{
+case c:
+count += print_character(arg);
+break;
+case d:
+case i:
+count += print_signInt(arg, 10);
+break;
+case s:
+count += print_string(arg);
+break;
+case x:
+count += print_base16_upper_lower(arg, "0123456789abcdef");
+break;
+case X:
+count += print_base16_upper_lower(arg, "0123456789ABCDEF");
+break;
+case p:
+count += print_addr(arg);
+break;
+case o:
+count += print_unsignedInt(arg, 8);
+break;
+case u:
+count += print_unsignedInt(arg, 10);
+break;
+default:
+count = -1;
+}
+return (count);
 }
---
+++ helper_functions.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to
+ * standard output
+ * @c: the character to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+
+}
+
+/**
+ * _puts - prints a string to standard output
+ * @str: pointer to address value
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != 0; i++)
+{
+_putchar(str[i]);
+}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer with the address value
+ *
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; s[i] != 0; i++)
+{
+;
+}
+return (i);
+}
+
+/**
+ * convert_to_format - convert from a format to another
+ * @rep: is a representation format 0123456789ABCDEF or 0123456789abcdef
+ * @number: unsigned int
+ * @base: base representation
+ * Return: is a pointer to string
+ */
+
+char *convert_to_format(char *rep, unsigned int number, int base)
+{
+char *str;
+
+static char buffer[128];
+
+int mod = 0;
+
+str = &buffer[127];
+
+*str = 0;
+
+do {
+mod = number % base;
+*--str = rep[mod];
+number /= base;
+} while (number != 0);
+return (str);
+}
